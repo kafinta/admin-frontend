b3c8e929a5ab89806284b932b570f0cef5e0e621
Qaudri <aquadirmuhammad@gmail.com>
2023-04-05 14:42:13 +0100

    chore: dashbaord overview design outline completed

---
+++ components/Ui/Cards/Overview.vue
@@ -0,0 +1,31 @@
+<template>
+  <nuxt-link :to="route" class="border border-accent-100 rounded-2xl p-5 flex items-center gap-5">
+    <img :src="imagePath" alt="" class="w-8 h-8">
+    <div>
+      <UiTypographyH2>{{ item_count }}</UiTypographyH2>
+      <UiTypographyP class="m-0">{{ item }}</UiTypographyP>
+    </div>
+  </nuxt-link>
+</template>
+<script>
+export default {
+  props: {
+    item_count: {
+      default: 10,
+      type: Number
+    },
+
+    item: {
+      default: 'Purchases',
+      type: String
+    },
+
+    route: {
+      default: '/dashboard',
+      type: String
+    },
+
+    imagePath: String,
+  }
+}
+</script>
--- pages/dashboard.vue
+++ pages/dashboard.vue
@@ -1,10 +1,61 @@
 <template>
   <div>
-    
+    <UiTypographyH2>Quick Insights</UiTypographyH2>
+    <div class="mt-6 grid grid-cols-2 lg:grid-cols-4 2xl:grid-cols-5 gap-5 flex-wrap">
+      <UiCardsOverview v-for="insight in insights" :key="insight.id" :imagePath="insight.imagePath" :item="insight.title" :item_count="insight.count" :route="insight.route" />
+    </div>
   </div>
 </template>
 <script>
 export default {
   layout: 'user',
+  data() {
+    return {
+      insights: [
+        {
+          id: 1,
+          imagePath: require('@/assets/images/insights/categories.svg'),
+          title: 'Categories',
+          count: '0',
+          route: '/categories'
+        },
+        {
+          id: 2,
+          imagePath: require('@/assets/images/insights/users.svg'),
+          title: 'Users',
+          count: '1000',
+          route: '/users'
+        },
+        {
+          id: 3,
+          imagePath: require('@/assets/images/insights/users.svg'),
+          title: 'Sellers',
+          count: '200',
+          route: '/users'
+        },
+        {
+          id: 4,
+          imagePath: require('@/assets/images/insights/users.svg'),
+          title: 'Buyers',
+          count: '800',
+          route: '/users'
+        },
+        {
+          id: 5,
+          imagePath: require('@/assets/images/insights/transactions.svg'),
+          title: 'Transactions',
+          count: '1328',
+          route: '/transactions'
+        },
+        {
+          id: 5,
+          imagePath: require('@/assets/images/insights/purchases.svg'),
+          title: 'Orders',
+          count: '20',
+          route: '/projects'
+        },
+      ]
+    }
+  },
 }
 </script>
