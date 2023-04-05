9096ffc81c6d2da9a3c174ab626f728bd74c522d
Qaudri <aquadirmuhammad@gmail.com>
2023-04-05 11:38:48 +0100

    chore: favicon added

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,7 +8,7 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'Kafinta | Home decor made easy',
+    title: 'Kafinta Admin',
     htmlAttrs: {
       lang: 'en'
     },
